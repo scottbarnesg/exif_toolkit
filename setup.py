b3a02c191e99484cf669584cade78c459fe01556
Scott Barnes <sgbarnes@protonmail.com>
2021-12-20 09:20:00 -0600

    Add README to setup.py long_description

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 import setuptools
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+README = (this_directory / "README.md").read_text()
+
 setuptools.setup(
     name="exif_toolkit",
     version='0.1.0',
     description='A set of tools for reading, modifying, and deleting exif data from images.',
+    long_description=README,
+    long_description_content_type='text/markdown',
     url='https://github.com/scottbarnesg/exif_toolkit',
     packages=setuptools.find_packages(),
     install_requires=["Pillow", "exif"],
